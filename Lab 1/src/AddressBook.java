780591d0960242203f0fb4a5bd730a013a09cee0
cdogg <camerongreer@cmail.carleton.ca>
2025-09-12 15:07:53 -0400

    Changed main method to create an AddressBook, add a BuddyInfo and remove the buddy

--- Lab 1/src/AddressBook.java
+++ Lab 1/src/AddressBook.java
@@ -5,6 +5,10 @@ public class AddressBook {
 
     public static void main(String[] args){
         System.out.println("Address Book");
+        AddressBook addressBook = new AddressBook();
+        BuddyInfo buddy1 = new BuddyInfo("Tom", "Carleton", 613);
+        addressBook.addBuddy(buddy1);
+        addressBook.removeBuddy(buddy1);
     }
 
 
